efe43ca3b867db7749e3af72e9b593460a5a7159
Jonathan Sanchez <jonathan.sanchez@windstream.net>
2025-03-07 08:59:21 -0700

    deploy

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 
-dotenv.config();  // Load environment variables
+dotenv.config(); // Load environment variables
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 53087;
@@ -14,16 +18,13 @@ app.use(express.json()); // Parse incoming JSON requests
 
 // Serve static React files
 if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/dist')));
 
-
-// Define Routes
-    
-app.use(express.static(path.join(__dirname, '/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist', 'index.html'));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+    });
 }
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  });
-  
+});
